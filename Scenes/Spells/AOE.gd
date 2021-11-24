e05044018a89cf39ab6c335356f2df5b114a70e9
Fredrik Peteri Harr <fredrik@peteri.se>
2021-11-24 16:06:58 +0100

    AOE damages mobs

--- Scenes/Spells/AOE.gd
+++ Scenes/Spells/AOE.gd
@@ -6,13 +6,11 @@ func spawn(position):
 	self.position = position
 	return self
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	timeout -= 1
 	if timeout < 0:
 		queue_free()
+
+func _on_AOE_body_entered(body):
+	if body.is_in_group("Hittable"):
+		body.hit("Hit by projectile")
